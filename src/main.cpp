69a6b720332fb18a8ddc4c798ce5a3dbbed4df87
vuhn-tree <rqvu.mail@gmail.com>
2023-01-27 23:32:05 -0800

    cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -26,11 +26,8 @@ void loop()
 #include <M5EPD.h>
 
 int _time = 0;
-
 M5EPD_Canvas canvas(&M5.EPD);
-/* After M5Paper is started or reset
-the program in the setUp () function will be run, and this part will only be run once.
-*/
+
 void setup()
 {
     M5.begin();
@@ -38,55 +35,34 @@ void setup()
     M5.EPD.Clear(true); // Clear the screen.
     M5.RTC.begin();     // Init the RTC.
     // canvas.createCanvas(540, 960);
+
     canvas.createCanvas(960, 540);
-    canvas.setTextSize(3);
+    canvas.setTextSize(20;
+    canvas.fillCanvas(0);
     // canvas.drawString("Hello World2", 45, 350);
     canvas.pushCanvas(0, 0, UPDATE_MODE_DU4); // Update the screen.
 }
-/* After the program in setup() runs, it runs the program in loop()
-The loop() function is an infinite loop in which the program runs repeatedly */
+
 void loop()
 {
-    if (millis() - _time > 1000) {
-        _time = millis();
-    char buf[130];
-    // buf[ptr] = '\0';
-    // if (ptr == 0) {
-    //     strcpy(buf, "Waiting...");
-    // }
-    // _time = millis();
-    rtc_time_t time_struct;
-    rtc_date_t date_struct;
-    M5.RTC.getTime(&time_struct);
-    M5.RTC.getDate(&date_struct);
-
-    if ((date_struct.year > 2010) && (time_struct.hour <= 24) &&
-        (time_struct.min <= 60) && (time_struct.sec <= 60))
+    if (millis() - _time > 1000)
     {
-        // pass_flag |= 0x01;
-    }
-    // if (_prev_sec == 255) {
-    //     _prev_sec = time_struct.sec;
-    // }
-    // if (time_struct.sec != _prev_sec) {
-    //     pass_flag |= 0x02;
-    // }
+        _time = millis();
+        char buf[130];
+        rtc_time_t time_struct;
+        rtc_date_t date_struct;
+        M5.RTC.getTime(&time_struct);
+        M5.RTC.getDate(&date_struct);
 
-    // _canvas_data->fillCanvas(0);
-    // _canvas_data->setTextSize(26);
+        // RTC
+        sprintf(buf, "%04d - %02d - %02d", date_struct.year, date_struct.mon,
+                date_struct.day);
+        canvas.drawString(buf, 30, 300);
+        sprintf(buf, "%02d : %02d : %02d", time_struct.hour, time_struct.min,
+                time_struct.sec);
+        canvas.drawString(buf, 50, 400);
 
-    canvas.fillCanvas(0);
-    canvas.setTextSize(26);
-
-    // RTC
-    sprintf(buf, "%04d - %02d - %02d", date_struct.year, date_struct.mon,
-            date_struct.day);
-    canvas.drawString(buf, 30, 300);
-    sprintf(buf, "%02d : %02d : %02d", time_struct.hour, time_struct.min,
-            time_struct.sec);
-    canvas.drawString(buf, 50, 400);
-
-    canvas.pushCanvas(0, 0, UPDATE_MODE_A2);
+        canvas.pushCanvas(0, 0, UPDATE_MODE_A2);
     }
 }
 
